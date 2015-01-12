d675cdb3f05f0d2b1dd79aeb195f777a995d36ad
Alessandro <alessandro@MBPdiAlessandro.e-xtrategy.intra>
2015-01-12 12:55:18 +0100

    file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,10 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // ReadMe Task
+    readme: {
+      
+    },
     // Uglify Task
     uglify: {
       build: {
@@ -10,16 +14,15 @@ module.exports = function(grunt) {
         dest: 'js/app.min.js'
       }
     },
-    // ReadMe Task
-    readme: {
-      
-    },
-    // Compass Task
-    compass: {
+    // minifyHtml Task
+    minifyHtml: {
+      options: {
+          cdata: true
+      },
       dist: {
-        options: {
-          config: 'config.rb'
-        }
+          files: {
+              'index.html': ['index_template.html']
+          }
       }
     },
     // CSSlint Task
@@ -30,20 +33,38 @@ module.exports = function(grunt) {
         },
         src: ['css/*.css']
       }
+    },
+    // Compass Task
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      },
+      dev: {
+        options: {
+          config: 'config.rb',
+          watch: true
+        }
+      }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   // Load the plugin grunt-readme
   grunt.loadNpmTasks('grunt-readme');
-  // Load the plugin compass
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugin minify-html
+  grunt.loadNpmTasks('grunt-minify-html');
   // Load the plugin CSSlint
   grunt.loadNpmTasks('grunt-contrib-csslint');
+  // Load the plugin compass
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
-  // Default task(s).
-  grunt.registerTask('default', ['readme', 'uglify', 'compass', 'csslint:strict']);
+  // Default task.
+  grunt.registerTask('default', ['readme', 'uglify', 'minifyHtml', 'csslint:strict', 'compass:dist']);
+  // Prod task.
+  grunt.registerTask('dev', ['csslint:strict', 'compass:dev']);
 
 
 };
