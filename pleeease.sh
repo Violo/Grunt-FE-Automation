75e33de4f09ce89c5e3168b4b0b1429162c53e9e
Francesco Strazzullo <me@francescostrazzullo.info>
2015-10-15 22:50:09 +0200

    using gruticon classes with sass

---
+++ pleeease.sh
@@ -0,0 +1,6 @@
+pleeease compile css/app.data.png.css
+mv css/app.min.css css/app.data.png.min.css
+pleeease compile css/app.data.svg.css
+mv css/app.min.css css/app.data.svg.min.css
+pleeease compile css/app.fallback.css
+mv css/app.min.css css/app.fallback.min.css
